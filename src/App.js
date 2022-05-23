c1ca315a51341b4f3abf851c1fd7977bdbdfa804
Assim <assimataya@gmail.com>
2022-05-23 13:42:02 +0100

    removed old code

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ const App = () => {
 
   // only excute if pokemon or searchField changes
   useEffect(() => {
-    console.log("use effect");
     const newFilteredPokemon = pokemon.filter((pokemon) => {
       return pokemon.name.toLocaleLowerCase().includes(searchField);
     });
@@ -52,70 +51,5 @@ const App = () => {
     </div>
   );
 };
-// This function is a component, a component is a self contained piece of code that returns a visual UI representation of html.
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       pokemon: [],
-//       searchField: "",
-//     };
-//     console.log("constructor");
-//   }
-
-//   // this method will run the code the moment the app first renders (mounts) to the page, it only happens once during an apps lifecycle.
-//   componentDidMount() {
-//     console.log("componentDidMount");
-//     fetch("https://pokeapi.co/api/v2/pokemon?limit=493")
-//       .then((response) => response.json())
-//       .then((response) =>
-//         Promise.all(
-//           response.results.map((element) =>
-//             fetch({ url: element.url }.url).then((response) => response.json())
-//           )
-//         )
-//       )
-//       .then((finalPokemonData) =>
-//         this.setState(
-//           () => {
-//             return { pokemon: finalPokemonData };
-//           },
-//           () => {
-//             console.log(this.state);
-//           }
-//         )
-//       );
-//   }
-
-//   onSearchChange = (event) => {
-//     const searchField = event.target.value.toLocaleLowerCase();
-//     this.setState(() => {
-//       return { searchField };
-//     });
-//   };
-
-//   render() {
-//     console.log("render from app.js");
-
-//     const { pokemon, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-//     const filteredPokemon = pokemon.filter((pokemon) => {
-//       return pokemon.name.toLocaleLowerCase().includes(searchField);
-//     });
-
-//     return (
-//       <div className="App">
-//         <h1 className="app-title">Ultimate Pokédex</h1>
-//         <SearchBox
-//           onChangeHandler={onSearchChange}
-//           placeHolder="search pokémon"
-//           className="pokemon-search-box"
-//         />
-//         <CardList pokemon={filteredPokemon} />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
